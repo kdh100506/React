0ee15f6b11f08a2486ba1f226a8e93f575d78ad0
kdh100506 <kdh100506@dgsw.hs.kr>
2026-08-31 21:38:13 +0900

    commit;

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,33 +1,12 @@
 import './App.css';
-import { useState } from 'react';
-
-const Bulb = ({ light }) => {
-  return <div>{light === 'ON' ? <h1 style={{ backgroundColor: 'orange' }}>ON</h1> : <h1 style={{ backgroundColor: 'gray' }}>OFF</h1>}</div>;
-};
+import Counter from './components/Counter';
+import Bulb from './components/Bulb';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
   return (
     <>
-      <div>
-        <Bulb light={light}></Bulb>
-        <button
-          onClick={() => {
-            setLight(light === 'ON' ? 'OFF' : 'ON');
-          }}
-        >
-          {light === 'ON' ? '끄기' : '켜기'}
-        </button>
-      </div>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
+      <Bulb></Bulb>
+      <Counter></Counter>
     </>
   );
 }
