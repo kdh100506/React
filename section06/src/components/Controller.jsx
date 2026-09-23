8caf6c029302982063f9f715b61c88167047a406
kdh100506 <kdh100506@dgsw.hs.kr>
2026-09-23 21:59:30 +0900

    section06

---
+++ section06/src/components/Controller.jsx
@@ -0,0 +1,56 @@
+function Controller({ onClickButton }) {
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          onClickButton(-1);
+        }}
+      >
+        -1
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          onClickButton(-10);
+        }}
+      >
+        -10
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          onClickButton(-100);
+        }}
+      >
+        -100
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          onClickButton(100);
+        }}
+      >
+        +100
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          onClickButton(10);
+        }}
+      >
+        +10
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          onClickButton(1);
+        }}
+      >
+        +1
+      </button>
+    </div>
+  );
+}
+
+export default Controller;
